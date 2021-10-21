be015b3554eaeb11835ef6a90e4b387af58e9511
Matt Williams <williams.mathew.t@gmail.com>
2021-10-21 00:56:07 -0400

    can each field in a struct a json tag.

--- rowan_parking_app/go/models/models.go
+++ rowan_parking_app/go/models/models.go
@@ -1,12 +1,12 @@
 //Author: Matt Williams
-//Version: 10/18/2021
+//Version: 10/19/2021
 //Go script that contains the stucture of our json and database formats.
 package models
 
 import (
 	"time"
 
-	geom "github.com/twpayne/go-geom"
+	"github.com/twpayne/go-geom/encoding/geojson"
 )
 
 type SettingsJson struct {
@@ -15,55 +15,55 @@ type SettingsJson struct {
 }
 
 type Venue struct {
-	Id            int64
-	VenueName     string
-	VenueLocation *geom.Point
+	Id            int64             `json:"Id"`
+	VenueName     string            `json:"VenueName"`
+	VenueLocation *geojson.Geometry `json:"VenueLocation"`
 }
 
 type Lot_Type struct {
-	Id       int64
-	TypeName string
-	Rules    string
+	Id       int64  `json:"Id"`
+	TypeName string `json:"TypeName"`
+	Rules    string `json:"Rules"`
 }
 
 type Lot struct {
-	Id             int64
-	LotName        string
-	LotDescription string
-	LotType        int64
-	NumSpaces      int64
-	VenueId        int64
-	SpecificRules  string
-	BoundingBox    *geom.Polygon
-	LotLocation    *geom.Point
+	Id             int64             `json:"Id"`
+	LotName        string            `json:"LotName"`
+	LotDescription string            `json:"LotDescription"`
+	LotType        int64             `json:"LotType"`
+	NumSpaces      int64             `json:"NumSpaces"`
+	VenueId        int64             `json:"VenueId"`
+	SpecificRules  string            `json:"SpecificRules"`
+	BoundingBox    *geojson.Geometry `json:"BoundingBox"`
+	LotLocation    *geojson.Geometry `json:"LotLocation"`
 }
 
 type Lot_Check_in struct {
-	Id           int64
-	LotId        int64
-	CheckInTime  time.Time
-	CheckOutTime time.Time
-	Userid       int64
+	Id           int64     `json:"Id"`
+	LotId        int64     `json:"LotId"`
+	CheckInTime  time.Time `json:"CheckInTime"`
+	CheckOutTime time.Time `json:"CheckOutTime"`
+	UserId       int64     `json:"Userid"`
 }
 
 type User struct {
-	Id          int64
-	Settings    SettingsJson
-	UserName    string
-	VenueId     int64
-	LastCheckIn int64
+	Id          int64        `json:"Id"`
+	Settings    SettingsJson `json:"Settings"`
+	UserName    string       `json:"UserName"`
+	VenueId     int64        `json:"VenueId"`
+	LastCheckIn int64        `json:"LastCheckIn"`
 }
 
 type Lot_Rating struct {
-	Id           int64
-	UserId       int64
-	LotId        int64
-	TimeOfReview time.Time
-	Review       int64
+	Id           int64     `json:"Id"`
+	UserId       int64     `json:"UserId"`
+	LotId        int64     `json:"LotId"`
+	TimeOfReview time.Time `json:"TimeOfReview"`
+	Review       int64     `json:"Review"`
 }
 
 type Bug struct {
-	Id        int64
-	BugReport string
-	UserId    int64
+	Id        int64  `json:"Id"`
+	BugReport string `json:"BugReport"`
+	UserId    int64  `json:"UserId"`
 }
