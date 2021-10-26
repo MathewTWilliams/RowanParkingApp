54db552381a729a02e947aff795c6fd817ed2926
Matt Williams <williams.mathew.t@gmail.com>
2021-10-26 18:53:23 -0400

    Small changes to api.go, updating go.mod and go.sum.

--- rowan_parking_app/go/api/api.go
+++ rowan_parking_app/go/api/api.go
@@ -41,10 +41,3 @@ func (api *API) StartListening() {
 	api.router.Run("localhost:80")
 
 }
-
-//func (ds *DataStore) CountSpotsTaken(vid int64, lid int64) int64 {
-//Use SelectQuerybuilder from select methods?
-//pass in count ('cols here') as a row parameter?
-//conditions: vid math, lid match, date of checkInTime in their timezone
-
-//}
--- rowan_parking_app/go/go.mod
+++ rowan_parking_app/go/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gin-gonic/gin v1.7.4
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/twpayne/go-geom v1.4.1
+	github.com/zsefvlol/timezonemapper v1.0.0
 )
 
 require (
