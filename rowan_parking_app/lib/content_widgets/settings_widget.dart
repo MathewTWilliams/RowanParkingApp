78259e71a3bfbc8ee9222e6adede46beeee13660
Andrew S <60792829+Zaffre23@users.noreply.github.com>
2021-11-09 20:45:46 -0500

    Update settings_widget.dart

--- rowan_parking_app/lib/content_widgets/settings_widget.dart
+++ rowan_parking_app/lib/content_widgets/settings_widget.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:settings_ui/settings_ui.dart';
-import 'package:shared_preferences_settings/shared_preferences_settings.dart';
+//import 'package:shared_preferences_settings/shared_preferences_settings.dart';
+
+import '../main.dart';
 
 bool darkModeToggled = false;
 bool value = false;
@@ -10,12 +14,16 @@ class SettingsWidget extends StatefulWidget {
 
   SettingsWidget({Key? key}) : super(key: key) {}
 
+
   @override
   State<StatefulWidget> createState() => SettingsWidgetState();
 }
 
 class SettingsWidgetState extends State<SettingsWidget> {
   static const darkModeKey = 'dark-mode';
+  bool isBusy = false;
+  bool isLoggedIn = true;
+
   @override
   Widget build(BuildContext context) => SettingsList(
     sections: [
@@ -45,11 +53,30 @@ class SettingsWidgetState extends State<SettingsWidget> {
               subtitle: 'Default',
               leading: Icon(Icons.sort_by_alpha),
               onPressed: (BuildContext context) {},
-            )
+            ),
+            SettingsTile(
+              title: 'Logout of app' ,
+              subtitle: 'Bye',
+              leading: Icon(Icons.logout),
+              onPressed: (BuildContext context) {() async {
+                //await logoutAction(); //Not working yet
+              };
+              //put in the navigation pop thing here
+              },
+          )
         ],
       )
     ]
   );
+
+  Future<void> logoutAction() async {
+    await secureStorage.delete(key: 'refresh_token');
+    setState(() {
+      isLoggedIn = false;
+      isBusy = false;
+    });
+  }
+
 }
 
 // admin panel icon thing: admin_panel_settings
