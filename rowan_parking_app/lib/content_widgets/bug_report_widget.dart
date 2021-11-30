9c9cbf7c163280b3ec7d9c3d151c3e1314a1180e
ZaireSims13 <71998196+ZaireSims13@users.noreply.github.com>
2021-11-30 14:48:31 -0500

    Update bug_report_widget.dart

--- rowan_parking_app/lib/content_widgets/bug_report_widget.dart
+++ rowan_parking_app/lib/content_widgets/bug_report_widget.dart
@@ -8,6 +8,52 @@ class BugReportWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: const Center(
+          child: MyStatefulWidget(),
+        ),
+      ),
+    );
   }
 }
+
+/// This is the stateful widget that the main application instantiates.
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+/// This is the private State class that goes with MyStatefulWidget.
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  String dropdownValue = 'Am not able to check into the lot';
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: dropdownValue,
+      style: const TextStyle(color: Color(0xFFFFCC00),fontSize: 17),
+      underline: Container(
+      ),
+      onChanged: (String? newValue) {
+        setState(() {
+          dropdownValue = newValue!;
+        });
+      },
+      items: <String>['Am not able to check into the lot','Am not able to check out of the lot','Parking lots are not showing up','Does not show how many available spots are left','Does not let me log out of the app','Other']
+          .map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    );
+  }
+}
+
+
+
